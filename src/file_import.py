64d0780def244d34ef8a8d81c802c3eaff349098
Wiltort <wiltort21@gmail.com>
2025-05-14 21:06:11 +0500

    readme created

--- src/file_import.py
+++ src/file_import.py
@@ -6,8 +6,10 @@ from src.data_processing import DataSet, DataError
 class FileError(Exception):
     pass
 
+
 class FileReader(ABC):
     """Класс чтения файлов"""
+
     type: str
 
     @abstractmethod
@@ -17,14 +19,15 @@ class FileReader(ABC):
     def file_is_valid(self, file: str):
         return "." in file and file.rsplit(".", 1)[1].lower() == self.type
 
-class CSVReader(FileReader):
-    type = 'csv'
 
-    def read_file(self, file: str, sep: str = ',') -> DataSet:
+class CSVReader(FileReader):
+    type = "csv"
+
+    def read_file(self, file: str, sep: str = ",") -> DataSet:
         if not self.file_is_valid(file):
-            raise FileError('Неверный формат файла')
+            raise FileError("Неверный формат файла")
         data = DataSet()
-        with open(file, 'r') as f: 
+        with open(file, "r") as f:
             data.set_fields(f.readline().strip().split(sep))
             for line in f.readlines():
                 data.add_record(line.strip().split(sep))
--- src/reports/report_types.py
+++ src/reports/report_types.py
@@ -4,22 +4,17 @@ from config import Config
 
 
 class PayoutReport(Report):
-    keyword = 'payout'
+    keyword = "payout"
     conf = Config.PAYOUT_REPORT_FIELDS
 
-
     def get_summary(self, data: DataSet) -> ReportDataSet:
-        fields=Config.PAYOUT_REPORT_FIELDS
+        fields = Config.PAYOUT_REPORT_FIELDS
         report = ReportDataSet()
         report = report.standardize(fields)
         for record in data.to_dict().values():
             report_record = []
-            for field in fields['existing_fields']:
+            for field in fields["existing_fields"]:
                 report_record.append(record[field])
             report_record.append(f"$ {record['rate'] * record['hours']}")
             report.add_record(report_record)
         return report
-
-
-
-
